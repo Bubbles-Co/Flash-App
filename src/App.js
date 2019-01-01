f3540649d656819520dcfe90e0bebe76093be8df
vishwa <vishwa2710@gmail.com>
2019-01-01 11:46:09 -0800

    Created some simple routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Route, BrowserRouter, Link } from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const Home = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>Welcome to Flash!</h1>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    </header>
+  </div>
+)
+
+const Routes = () => (
+  <div>
+    <h1>This where the routes you have done go</h1>
+  </div>
+)
+
+const Sessions = () => (
+  <div>
+    <h1>This is where you will input your sessions</h1>
+  </div>
+)
+
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route path='/routes' component={Routes}/>
+      <Route path='/sessions' component={Sessions}/>
+    </Switch>
+  </main>
+)
+
+const Header = () => (
+  <header>
+    <nav>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/routes'>Routes</Link></li>
+        <li><Link to='/sessions'>Sessions</Link></li>
+      </ul>
+    </nav>
+  </header>
+)
+
+const AppRoute = () => (
+  <div>
+    <Header />
+    <Main />
+  </div>
+)
+
+const App = () => (
+  <div className="App">
+    <BrowserRouter>
+      <AppRoute />
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
